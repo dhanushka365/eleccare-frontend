784ced7ad5adaa8834ceb2800e4ad618452c0f4c
dhanushka365 <technohub10000@gmail.com>
2023-02-08 10:28:38 +0530

    side pannel completed

---
+++ src/assets/dummy-data/navLinks.js
@@ -0,0 +1,24 @@
+const navLinks = [
+    {
+      path: "/dashboard",
+      icon: "ri-apps-2-line",
+      display: "Dashboard",
+    },
+    {
+      path: "/relays",
+      icon: "ri-taxi-line",
+      display: "Relays",
+    },
+    {
+      path: "/sensors",
+      icon: "ri-shopping-bag-line",
+      display: "Sensors",
+    },
+    {
+      path: "/settings",
+      icon: "ri-settings-2-line",
+      display: "Settings",
+    },
+  ];
+  
+  export default navLinks;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Dashboard from "../pages/Dashboard";
 import Relays from "../pages/Relays";
@@ -7,7 +7,7 @@ import Sensors from "../pages/Sensors";
 import Settings from "../pages/Settings";
 
 const Router = () => {
-  return <BrowserRouter>
+  return (
     <Routes>
         <Route path="/" element={<Navigate to="/dashboard" element={<Dashboard />} />}/>
         <Route path="/dashboard" element={<Dashboard />} />
@@ -15,7 +15,7 @@ const Router = () => {
         <Route path="/sensors" element={<Sensors />} />
         <Route path="/settings" element={<Settings />} />
      </Routes>
-    </BrowserRouter>
+  );
 };
 
 export default Router;
