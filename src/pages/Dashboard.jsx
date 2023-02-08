9b1fb7d33d4673e4ba0c2ba6364be1ffcb86be6a
dhanushka365 <technohub10000@gmail.com>
2023-02-08 15:16:28 +0530

    no errors

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import "../styles/dashboard.css";
+import SingleCard from "../components/reuseable/SingleCard";
+
+import MileChart from "../charts/MileChart";
+import CarStatsChart from "../charts/CarStatsChart";
+
+const voltageObj = {
+    title: "Volatage",
+    totalNumber: 230,
+    icon: "ri-police-car-line",
+  };
+  
+  const currentObj = {
+    title: "Current",
+    totalNumber: 1697,
+    icon: "ri-steering-2-line",
+  };
+  
+  const powerObj = {
+    title: "Power",
+    totalNumber: "85k",
+    icon: "ri-user-line",
+  };
+  
+  const distanceObj = {
+    title: "Cost Daily",
+    totalNumber: 2167,
+    icon: "ri-timer-flash-line",
+  };
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+   return (
+    <div className="dashboard">
+      <div className="dashboard__wrapper">
+        <div className="dashboard__cards">
+          <SingleCard item={voltageObj} />
+          <SingleCard item={currentObj} />
+          <SingleCard item={powerObj} />
+          <SingleCard item={distanceObj} />
+        </div>
+
+        <div className="statics">
+          <div className="stats">
+            <h3 className="stats__title">Energy Statistics</h3>
+            <MileChart />
+          </div>
+
+          <div className="stats">
+            <h3 className="stats__title">Forecast Statistics</h3>
+            <CarStatsChart />
+          </div>
+        </div>
+
+    
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/pages/Relays.jsx
+++ src/pages/Relays.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React from "react"; 
 
+
+//src\assets\dummy-data\elecdata.json
 const Relays = () => {
+  
   return <div>Relays</div>;
 };
 
